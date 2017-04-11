d6619662b36981eb17b34cfe5e9b9d7114d71aec
schwifty <jcmcmanemin@gmail.com>
2017-04-11 02:44:20 -0500

    individual post pages and dynamic urls

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import jinja2
 import os
 from google.appengine.ext import db
 
+
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
@@ -32,6 +33,7 @@ class Index(Handler):
 class NewPost(Handler):
   def get(self):
     error = ""
+    error = cgi.escape(error)
     t = jinja_env.get_template('newpost.html')
     content = t.render(error = error)
     self.response.write(content)
@@ -39,17 +41,39 @@ class NewPost(Handler):
   def post(self):
     post_title = self.request.get('post-title')
     post_content = self.request.get('post-content')
-    post_title_esc = cgi.escape(post_title)
-    post_content_esc = cgi.escape(post_content)
+    post_title_esc = cgi.escape(post_title, quote=True)
+    post_content_esc = cgi.escape(post_content, quote=True)
     
-    post = Post(title = post_title_esc, content = post_content_esc)
-    post.put()
+    if not post_title_esc or post_content_esc:
+      error = "Please enter a title and content"
+      t = jinja_env.get_template('newpost.html')
+      content = t.render(error = cgi.escape(error))
+      self.response.write(content)
     
-    t = jinja_env.get_template('post-confirmation.html')
-    content = t.render()
-    self.response.write(content)
+    if post_title_esc and post_content_esc:
+      post = Post(title = post_title_esc, content = post_content_esc)
+      post.put()
+      post_id = post.key().id()
+      
+      self.redirect('/blog/' + str(post_id))
+      
     
-    
+class ViewPost(Handler):
+    def get(self, id):
+      post = Post.get_by_id(int(id))
+      t = jinja_env.get_template('post.html')
+      content = t.render(post = post)
+      self.response.write(content)
+
+class Redirect(Handler):
+  def get(self):
+    self.redirect('/blog')
+# class Confirmation(Handler):
+#   def get(self):
+#     t = jinja_env.get_template('post-confirmation.html')
+#     content = t.render()
+#     self.response.write(content)
+
 app = webapp2.WSGIApplication([
-    ('/', Index), ('/newpost', NewPost)
+    ('/blog', Index), ('/blog/newpost', NewPost), webapp2.Route('/blog/<id:\d+>', ViewPost), ('/', Redirect)
 ], debug=True)
